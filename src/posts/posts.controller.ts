bc713da84e57a2d38ccce260455f7330a5c4fb2d
vijay <vijay.11sah@gmail.com>
2021-07-11 15:58:33 +0530

    adds endpoints in post controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 
-@Controller('posts')
-export class PostsController {}
+@Controller("posts")
+export class PostsController {
+  @Get("/")
+  getAllPosts(): string {
+    return "All Posts";
+  }
+
+  @Get("/:postId")
+  getPostDetails(@Param() param): string {
+    return `Details of Post Id: ${param.postId}`;
+  }
+
+  @Post("/")
+  createNewPost(): string {
+    return `New Post Created`;
+  }
+
+  @Delete("/:postId")
+  deletePost(@Param() param): string {
+    return `Post with id ${param.postId} deleted`;
+  }
+
+  @Put("/:postId")
+  likePost(@Param() param): string {
+    return `Liked Post ${param.postId}`
+  }
+
+  @Delete("/:postId")
+  unlikePost(@Param() param): string {
+    return `unLiked Post ${param.postId}`
+  }
+}
